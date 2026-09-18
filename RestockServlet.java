431c894f52a58b6d4b9c9214f920ade493bd9ba5
Anuvardhitha <anuvardhithavinukonda@gmail.com>
2026-09-18 21:00:51 +0530

    Update RestockServlet.java

--- RestockServlet.java
+++ RestockServlet.java
@@ -17,7 +17,7 @@ public class RestockServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
         int productId = Integer.parseInt(request.getParameter("productId"));
