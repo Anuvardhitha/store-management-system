bc7412f9d32695c45af0ed9c5ef96c5b187098cd
Anuvardhitha <anuvardhithavinukonda@gmail.com>
2026-09-19 20:31:43 +0530

    Update AddProductServlet.java

--- AddProductServlet.java
+++ AddProductServlet.java
@@ -23,9 +23,9 @@ private static final String CSS =
         int quantity = Integer.parseInt(request.getParameter("quantity"));
         int minStock = Integer.parseInt(request.getParameter("minStock"));
 
-        String url = "jdbc:postgresql://dpg-da3cm36k1f9s73ejv0j0-a.ohio-postgres.render.com:5432/store_db_8wgw";
-String user = "store_db_8wgw_user";
-String password = "xybXiHQe8UpL4DEEpNXjHd8ujdDvEMjJ";
+       String url = System.getenv("DB_URL");
+String user = System.getenv("DB_USER");
+String password = System.getenv("DB_PASSWORD");
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
